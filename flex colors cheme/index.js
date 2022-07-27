8244f280b23d61ed38da82b4bbcb2648882647c6
pietheking <104598311+pietheking@users.noreply.github.com>
2022-07-27 16:17:18 +0100

    Improved design, functionality and components
    I improved its functionality by adding error messages and as well making it have a Hex code block;

--- flex colors cheme/index.js
+++ flex colors cheme/index.js
@@ -1,13 +1,26 @@
 const baseColor=document.getElementById('base-color')
 const mode=document.getElementById('mode');
 const pArr=document.getElementsByTagName('p')
-function getScheme(){
-fetch(`https://www.thecolorapi.com/scheme?hex=${(baseColor.value).slice(1,7)}&mode=${mode.value.toLowerCase()}`)
-.then(res=>res.json())
-.then(data=>{for(cols in data.colors){document.getElementsByClassName('dj')[cols].style.backgroundColor=data.colors[cols].hex.value 
-pArr[cols].textContent=data.colors[cols].hex.value}})
-}
+const colorText=document.getElementById('color-text')
+let id;
+colorText.value=baseColor.value
 getScheme()
+baseColor.addEventListener("click",()=>{id=setInterval(()=>colorText.value=baseColor.value,1)})
+colorText.addEventListener("click",()=>{clearInterval(id)})
+function getScheme(){
+    if(colorText.value[0]!='#'){
+        colorText.value='#'+colorText.value
+    }
+    fetch(`https://www.thecolorapi.com/scheme?hex=${(colorText.value).slice(1,7)}&mode=${mode.value.toLowerCase()}`)
+        .then(res=>res.json())
+        .then(data=>{for(cols in data.colors){document.getElementsByClassName('dj')[cols].style.backgroundColor=data.colors[cols].hex.value 
+        pArr[cols].textContent=data.colors[cols].hex.value}
+        console.log(pArr[1].textContent)})
+        if(pArr[1].textContent==='#000000'){
+        alert('please hex code only contain numbers and alphabets from "a-f"')
+        }
+
+}
 for(ele of pArr){
     ele.addEventListener('click',()=>{
         document.execCommand('copy')
